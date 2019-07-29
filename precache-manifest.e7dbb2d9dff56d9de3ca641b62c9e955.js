0f78ba950ef050244c031f51141fc573af056821
Joanne Badua <joannebadua@gmail.com>
2019-07-28 21:26:01 -0700

    Updates

---
+++ precache-manifest.e7dbb2d9dff56d9de3ca641b62c9e955.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6a9172a29b0008b17747daca377d9e9",
+    "url": "/reading-rainbow-google-api/index.html"
+  },
+  {
+    "revision": "2ed2000ffe6db73a88f3",
+    "url": "/reading-rainbow-google-api/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/reading-rainbow-google-api/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "2ed2000ffe6db73a88f3",
+    "url": "/reading-rainbow-google-api/static/js/main.15f21000.chunk.js"
+  },
+  {
+    "revision": "e5a16432104498f51e14",
+    "url": "/reading-rainbow-google-api/static/js/runtime~main.cd87d73e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reading-rainbow-google-api/static/media/logo.5d5d9eef.svg"
+  }
+]);
